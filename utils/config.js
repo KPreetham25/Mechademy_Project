6bb4353508ed7f9d7221e096be6a2779f05af893
Preetham Kalpuri <kalpuripreetham67@gmail.com>
2022-07-31 19:06:01 +0530

    added task  in config js

--- utils/config.js
+++ utils/config.js
@@ -4,3 +4,4 @@ exports.dbUrl = 'mongodb://localhost:27017';
 exports.apiKey = 'BJIPIDuWMR3qK6lzQgYQmjNq7eh8QpR6BofUeYIDGBEx';
 exports.author = 'Mechademy Author';
 exports.company = 'Mechademy';
+exports.task = 'devops challenge';
